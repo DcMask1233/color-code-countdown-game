9009bf444557f63fcc461f25718da8e1a6026726
DcMask1233 <saisajeev000@gmail.com>
2025-06-18 15:18:09 -0400

    Create UserBet.ts

---
+++ src/types/UserBet.ts
@@ -0,0 +1,9 @@
+export interface UserBet {
+  period: string;
+  betType: 'color' | 'number';
+  betValue: string | number;
+  amount: number;
+  result?: 'win' | 'lose';
+  payout?: number;
+  timestamp: Date;
+}
