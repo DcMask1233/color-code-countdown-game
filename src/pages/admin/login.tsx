4c431e3b70a3b5e9b2834832d061bc1f2ae1dd8d
DcMask1233 <saisajeev000@gmail.com>
2025-06-19 02:59:01 -0400

    Update login.tsx

--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -17,7 +17,7 @@ export default function AdminLogin() {
 
     if (error) {
       alert("Login failed: " + error.message);
-    } else if (data?.user?.email === "youradmin@email.com") {
+    } else if (data?.user?.email === "dcmask@gmail.com") {
       router.push("/admin"); // go to admin dashboard
     } else {
       alert("Not an admin user");
